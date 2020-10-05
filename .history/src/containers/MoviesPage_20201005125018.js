ba7c1a6210c5942a899414577d1eaecfa30b1540
Charlie Pelton <charliejp0311@gmail.com>
2020-10-05 12:53:58 -0500

    Done.

--- .history/src/containers/MoviesPage_20201005125018.js
+++ .history/src/containers/MoviesPage_20201005125018.js
@@ -7,6 +7,7 @@ import MovieShow from '../components/MovieShow';
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
+    // Adding code to show a message to the user to select a movie if they haven't yet
     <Route exact path={match.url} render={() => <h3>Choose a movie from the list above</h3>}/>
     <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow {...routerProps} movies={movies} /> }/>
   </div>
